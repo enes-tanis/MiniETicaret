8830c08bc786a2f763cb4d8a9f5416cc9153d0f0
Enes Tanış <aenes.tanis@gmail.com>
2023-11-26 18:49:13 +0300

    Set up the DbContext and Repository Pattern

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Repositories/IWriteReopsitory.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteReopsitory<T>:IRepository<T> where T : BaseEntity  
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddASync(List<T> model);
+        Task<bool> Remove(T model);
+        Task<bool> Remove(string id);
+        Task<bool> UpdateAsync(T model);
+    }
+}
