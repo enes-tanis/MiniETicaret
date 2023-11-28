89e2076ff9ddc1666e8b35237405d21776f89dd5
Enes Tanış <aenes.tanis@gmail.com>
2023-11-28 03:23:00 +0300

    Development of Admin and UI Infrastructure

--- ETicaretClient/src/app/ui/ui.module.ts
+++ ETicaretClient/src/app/ui/ui.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from './layout/layout.module';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, LayoutModule, ComponentsModule],
-  exports: [LayoutModule],
+  imports: [CommonModule, ComponentsModule],
 })
-export class AdminModule {}
+export class UiModule {}
