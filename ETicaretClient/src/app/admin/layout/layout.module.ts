e011a6f39814a47a656dfdcb1a70bfcf83bd8d09
Enes Tanış <aenes.tanis@gmail.com>
2023-11-28 15:00:42 +0300

    Angular Multiple Layout Infrastructure Construction and Routing structure establishment

--- ETicaretClient/src/app/admin/layout/layout.module.ts
+++ ETicaretClient/src/app/admin/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, ComponentsModule],
-  exports: [LayoutComponent],
+  imports: [CommonModule, ComponentsModule, RouterModule],
+  exports: [],
 })
 export class LayoutModule {}
