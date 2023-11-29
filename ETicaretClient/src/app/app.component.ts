933735bb92959190c48141e0100dda5b9a04b630
Enes Tanış <aenes.tanis@gmail.com>
2023-11-30 02:47:14 +0300

    Angular Material UI and Bootsrap entegration

--- ETicaretClient/src/app/app.component.ts
+++ ETicaretClient/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ETicaretClient';
-}
+export class AppComponent {}
+$(document).ready(() => {
+  alert('hello');
+});
