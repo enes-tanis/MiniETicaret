f14aa04711a0ee6e65bba47907353af578a8a4ec
Enes Tanış <aenes.tanis@gmail.com>
2023-12-03 17:09:02 +0300

    Customising Toastr for ui

--- ETicaretClient/src/app/app.component.ts
+++ ETicaretClient/src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from './services/ui/custom-toastr.service';
 declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private toastr: CustomToastrService) {
+    this.toastr.message('Hello world', 'title', {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopRight,
+    });
+  }
+}
